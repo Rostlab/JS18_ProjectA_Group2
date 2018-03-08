a35b2eaa966c953cf61fcfab6c0d625e162ba283
Carsten Sehlke <carsten.sehlke@tum.de>
2018-03-08 12:54:49 +0100

    create subchart components

---
+++ UI/src/app/chart/scatter-plot/scatter-plot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scatter-plot',
+  templateUrl: './scatter-plot.component.html',
+  styleUrls: ['./scatter-plot.component.css']
+})
+export class ScatterPlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
