aa5e2b6f2e69d3f69c0d032e47d33348e4cb991c
JyotirmayS <j.senapati@tum.de>
2018-03-13 23:26:08 +0100

    Update NodeIndexService.js

--- server/services/NodeIndexService.js
+++ server/services/NodeIndexService.js
@@ -14,15 +14,6 @@ var NodeIndexService = {
 	sayHi: function (next) {
         next(null, "Welcome to iGraph");
     },
-    loadCSV: function (next){
-        const csvFilePath="server/data/core_dataset.csv";
-        var df = DF
-        .readFileSync(csvFilePath)
-        .parseCSV();
-        console.log(df);
-
-        next(null, df);
-    },
     nlp: function(userquery, next){
         var propertiesObject = { q:userquery, model:'sample_igraph' };
 
