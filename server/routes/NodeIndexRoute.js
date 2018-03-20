3453ce63f0a9d84426073ffb744313391fa2b04a
iraklitchedia <ga83maw@mytum.de>
2018-03-20 09:28:01 +0100

    github issue 48: "Create file upload". Added file upload backend

--- server/routes/NodeIndexRoute.js
+++ server/routes/NodeIndexRoute.js
@@ -7,6 +7,25 @@ var express = require('express');
 var indexService = require("../services/NodeIndexService")
 var router = express.Router();
 var qs = require('querystring');
+var multer = require('multer');
+var path = require('path');
+const crypto = require('crypto');
+
+var DIR = "./server/data/";
+var storage = multer.diskStorage({
+  destination: DIR,
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
+});
+
+
+
+var upload = multer({storage: storage}).single('fileItem');
 
 console.log('in NodeIndexRoute');
 
@@ -65,17 +84,19 @@ router.get('/columns', function (req, res, next) {
 });
 
 // optinal feature to implement, if time permits.
-/*router.post('/upload', function (req, res, next) {
-  console.log(req.query);
-  indexService.upload(req.query, function (err, data) {
-    
-      if (err) {
-        res.send(err);
-        return;
-      } else {
-        res.send(data);
-        return;
-      }
-    })
-});*/
+router.post('/upload', function (req, res, next) {
+  var path = "";
+
+  upload(req, res, function (err) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+      return;
+    } else {
+      res.send("File upload complete " + req.file.path);
+      return;
+    }
+  })
+});
+
 module.exports = router;
