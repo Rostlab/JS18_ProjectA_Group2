554d1882592930ec382edf075bae34eeba060679
iraklitchedia <ga83maw@mytum.de>
2018-04-05 22:49:49 +0200

    Fixed read date values from csv bug

--- server/utils/csvtomysql.js
+++ server/utils/csvtomysql.js
@@ -14,6 +14,8 @@ let csvToMysql = {
                     db: "mysql://" + mysql.user + ":" + mysql.password + "@" + mysql.host + ":" + mysql.port +
                     "/" + mysql.database,
                     tableName: table,
+                    dateFormat: ["MM/DD/YYYY", "M/D/YYYY"],
+                    datetimeFormat: ["MM/DD/YYYY HH:mm:ss a", "M/D/YYYY HH:mm:ss a"]
                 });
 
                 //TODO: Remove timeout with proper setup for promise handling(resolving).
