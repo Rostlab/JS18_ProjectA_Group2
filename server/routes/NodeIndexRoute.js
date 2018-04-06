3dc6193246059a05b514eecf53835f1956969c25
Jyotirmay Senapati <senapati.jyotirmay@gmail.com>
2018-04-06 15:25:00 +0200

    README Update

--- server/routes/NodeIndexRoute.js
+++ server/routes/NodeIndexRoute.js
@@ -10,7 +10,7 @@ var router = express.Router();
 var path = require('path');
 
 
-router.get('/test', function (req, res) {
+router.get('/', function (req, res) {
     res.send("Welcome to iGraph");
 });
 
