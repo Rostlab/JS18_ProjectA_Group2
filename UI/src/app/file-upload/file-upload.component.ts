cd573ebf5cf665070ad56e694d99201ae9a3b938
Carsten Sehlke <carsten.sehlke@tum.de>
2018-04-07 17:31:48 +0200

    add test cases to backend connector in the frontend

--- UI/src/app/file-upload/file-upload.component.ts
+++ UI/src/app/file-upload/file-upload.component.ts
@@ -4,9 +4,9 @@ import { BackendConnectorService } from "../services";
 import { Dataset} from '../../../models';
 
 @Component({
-  selector: 'app-file-upload',
-  templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.css']
+    selector: 'app-file-upload',
+    templateUrl: './file-upload.component.html',
+    styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit, OnDestroy {
     fileSelected: boolean = true;
@@ -20,7 +20,7 @@ export class FileUploadComponent implements OnInit, OnDestroy {
     constructor(private fileUploadService: BackendConnectorService) {}
 
     ngOnInit() {
-      this.statusFormGroup = new FormGroup({});
+        this.statusFormGroup = new FormGroup({});
     }
 
     @Input() dataList;
@@ -33,41 +33,41 @@ export class FileUploadComponent implements OnInit, OnDestroy {
     }
 
     handleSubmit(event:any, statusNgForm:NgForm, statusFormGroup: FormGroup){
-      event.preventDefault()
-      if (statusNgForm.submitted){
-          let submittedData = statusFormGroup.value;
+        event.preventDefault();
+        if (statusNgForm.submitted){
+            let submittedData = statusFormGroup.value;
+            console.log(submittedData);
 
-          this.fileUploadSub = this.fileUploadService.fileUpload(
-                this.fileToUpload, 
-                submittedData).subscribe(
-                    event=>{ this.handleResponse(event); }, 
+            this.fileUploadSub = this.fileUploadService.fileUpload(this.fileToUpload, submittedData)
+                .subscribe(
+                    event=>{ this.handleResponse(event); },
                     error=>{
                         console.log( error.error);
                     });
 
-          statusNgForm.resetForm({});          
-      }
-  }
-
-  handleResponse(response){
-    if(response.fileName){      
-      var fileName = response.fileName;
-
-      //Insert new file into dataset selection list
-      var id = this.dataList[this.dataList.length - 1].id + 1;
-      this.dataList.push(new Dataset(id, fileName));
-      window.alert(response.fileName+" has been uploaded to database!");
-    }else{
-      window.alert(response.Error);
+            statusNgForm.resetForm({});
+        }
     }
-  }
+
+    handleResponse(response){
+        if(response.fileName){
+            var fileName = response.fileName;
+
+            //Insert new file into dataset selection list
+            var id = this.dataList[this.dataList.length - 1].id + 1;
+            this.dataList.push(new Dataset(id, fileName));
+            window.alert(response.fileName+" has been uploaded to database!");
+        }else{
+            window.alert(response.Error);
+        }
+    }
 
 
     handleFileInput(files: FileList) {
         let fileItem = files.item(0);
         console.log("file input has changed. The file is", fileItem);
         this.fileToUpload = fileItem;
-        
+
         //If file was selected enable submit button else disable
         this.fileSelected = fileItem ? false : true;
     }
--- UI/src/app/services/backend-connector.service.spec.ts
+++ UI/src/app/services/backend-connector.service.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { BackendConnectorService } from './backend-connector.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import {Dataset} from "../../../models";
+import {BackendConnectorService} from './backend-connector.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {Columns, Data, Dataset} from "../../../models";
 
 describe('BackendConnectorService', () => {
     let service: BackendConnectorService;
@@ -26,7 +26,96 @@ describe('BackendConnectorService', () => {
         expect(service).toBeTruthy();
     }));
 
-    it('should retrieve names of data tables from the API via GET', () => {
+    /*fit('should get data to plot from the API via GET', () => {
+        const dummyPosts: QueryResponse = {
+            x: [1,2,3],
+            x_title: "X",
+            y: [4,5,6],
+            y_title: "Y",
+            plot_type: "bar",
+            delta: [1,3,5],
+            delta_title: "D",
+            title: "Tit"
+        };
+        const dummyQueryResponse: Data = {
+            traces: [{
+                type: "bar",
+                x: [1,2,3],
+                xaxis: "X",
+                y: [4,5,6],
+                yaxis: "Y",
+                labels: [1,2,3],
+                values: [4,5,6]
+            }],
+            layout: {},
+            options: {}
+        };
+
+        service.getData("DummyPlotRequest", "DatasetName").subscribe((posts: Data) => {
+            console.log(dummyQueryResponse);
+            expect(posts).toBe(dummyQueryResponse);
+        });
+
+        const request = httpMock.expectOne(request => request.url === '/api/plot');
+
+        expect(request.request.url).toEqual('/api/plot');
+        expect(request.request.method).toEqual('GET');
+
+        request.flush(dummyPosts);
+    });*/
+
+    fit('should get update from the API via GET', () => {
+        const dummyResponse: any[] = [
+            {action: "", value: "", trace: []},
+            {action: "", value: "", trace: []}
+        ];
+        const dummyChart: Data = {
+            traces: [{
+                x: [],
+                y: [],
+                xaxis: "",
+                yaxis: "",
+                values: [],
+                labels: [],
+                type: 'bar'
+            }],
+            layout: {
+            },
+            options: {
+            }
+        };
+
+        service.update("DummyChangeRequest", dummyChart).subscribe((posts: any[]) => {
+            expect(posts.length).toEqual(2);
+            expect(posts).toEqual(dummyResponse);
+        });
+
+        const request = httpMock.expectOne(request => request.url === 'https://js2018-group4.azurewebsites.net/api/nlp');
+
+        expect(request.request.url).toEqual('https://js2018-group4.azurewebsites.net/api/nlp');
+        expect(request.request.method).toEqual('POST');
+
+        request.flush(dummyResponse);
+    });
+
+    /*fit('should upload file to the API via POST', () => {
+        const dummyPosts: any = {
+            filename: 'Value 1'
+        };
+
+        service.fileUpload(new File([], "DummyFilename")).subscribe(event => {
+            expect(event).toEqual(dummyPosts);
+        });
+
+        const request = httpMock.expectOne(request => request.url === '/api/getTables');
+
+        expect(request.request.url).toEqual('/api/getTables');
+        expect(request.request.method).toEqual('POST');
+
+        request.flush(dummyPosts);
+    });*/
+
+    fit('should retrieve names of data tables from the API via GET', () => {
         const dummyPosts: Dataset[] = [
             {id: 1, name:'Dataset 1'},
             {id: 2, name:'Dataset 2'}
@@ -37,31 +126,30 @@ describe('BackendConnectorService', () => {
             expect(posts).toEqual(dummyPosts);
         });
 
-        const request = httpMock.expectOne('/api/getTables');
+        const request = httpMock.expectOne(request => request.url === '/api/getTables');
 
         expect(request.request.url).toEqual('/api/getTables');
         expect(request.request.method).toEqual('GET');
-        // expect(request.request.headers.get('Content-Type')).toEqual('application/json');
 
         request.flush(dummyPosts);
     });
 
-    it('should upload file to the API via POST', () => {
-        const dummyPosts: Dataset[] = [
-            {id: 1, name:'Dataset 1'},
-            {id: 2, name:'Dataset 2'}
-        ];
+    fit('should get columns from the API via GET', () => {
+        const dummyPosts: Columns = {
+            columns:[
+                'Value 1',
+                'Value 2'
+            ]
+        };
 
-        service.getDatabaseTables().subscribe(posts => {
-            expect(posts.length).toBe(2);
+        service.getColumns("testTable").subscribe(posts => {
             expect(posts).toEqual(dummyPosts);
         });
 
-        const request = httpMock.expectOne('/api/getTables');
+        const request = httpMock.expectOne(request => request.url === '/api/columns');
 
-        expect(request.request.url).toEqual('/api/getTables');
+        expect(request.request.url).toEqual('/api/columns');
         expect(request.request.method).toEqual('GET');
-        // expect(request.request.headers.get('Content-Type')).toEqual('application/json');
 
         request.flush(dummyPosts);
     });
