5de7e737ef92039f47ee45328734029ee888fe3c
Carsten Sehlke <carsten.sehlke@tum.de>
2018-04-08 18:50:45 +0200

    remove primKey from column list

--- server/config.js
+++ server/config.js
@@ -17,7 +17,7 @@ let config = (function() {
             host: 'localhost',
             port: '3306',
             user: 'root',
-            password: '',
+            password: 'root',
             database: 'igraph'
         },
 
