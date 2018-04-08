8b4b31554e01c59ca438514c3468e6cbf2cf1a24
Carsten Sehlke <carsten.sehlke@tum.de>
2018-04-08 12:30:29 +0200

    add test cases for home component

--- UI/src/app/home/home.component.spec.ts
+++ UI/src/app/home/home.component.spec.ts
@@ -6,6 +6,7 @@ import { TextInputComponent } from '../text-input/text-input.component';
 import { ChartComponent } from '../chart/chart.component';
 import { BackendConnectorService } from "../services";
 import { FileUploadComponent } from '../file-upload/file-upload.component';
+import { Columns, Dataset } from "../../../models";
 
 describe('HomeComponent', () => {
     let component: HomeComponent;
@@ -40,4 +41,176 @@ describe('HomeComponent', () => {
     fit('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    fit('shouldDisablePlotButton(): text field empty and not default dataset', () => {
+        component.textInput.textInput = "";
+        component.dataset = new Dataset(-5, "Test");
+        fixture.detectChanges();
+        expect(component.shouldDisablePlotButton()).toBeTruthy();
+    });
+
+    fit('shouldDisablePlotButton(): text field not empty and not default dataset', () => {
+        component.dataset = new Dataset(-5, "Test");
+        component.textInput.textInput = "test";
+        fixture.detectChanges();
+        expect(component.shouldDisablePlotButton()).toBeFalsy();
+    });
+
+    fit('shouldDisablePlotButton(): text field not empty and default dataset', () => {
+        component.textInput.textInput = "test";
+        component.dataset = component.defaultDataset;
+        fixture.detectChanges();
+        expect(component.shouldDisablePlotButton()).toBeTruthy();
+    });
+
+    fit('shouldDisablePlotButton(): text field empty and default dataset', () => {
+        component.dataset = component.defaultDataset;
+        component.textInput.textInput = "";
+        fixture.detectChanges();
+        expect(component.shouldDisablePlotButton()).toBeTruthy();
+    });
+
+    fit('shouldDisableUpdateButton(): text field empty and graph empty', () => {
+        component.textInputUpdate.textInput = "";
+        component.graphIsEmpty = false;
+        fixture.detectChanges();
+        expect(component.shouldDisableUpdateButton()).toBeTruthy();
+    });
+
+    fit('shouldDisableUpdateButton(): text field not empty and graph empty', () => {
+        component.graphIsEmpty = false;
+        component.textInputUpdate.textInput = "test";
+        fixture.detectChanges();
+        expect(component.shouldDisableUpdateButton()).toBeFalsy();
+    });
+
+    fit('shouldDisableUpdateButton(): text field not empty and graph not empty', () => {
+        component.textInputUpdate.textInput = "test";
+        component.graphIsEmpty = true;
+        fixture.detectChanges();
+        expect(component.shouldDisableUpdateButton()).toBeTruthy();
+    });
+
+    fit('shouldDisableUpdateButton(): text field empty and graph not empty', () => {
+        component.graphIsEmpty = true;
+        component.textInputUpdate.textInput = "";
+        fixture.detectChanges();
+        expect(component.shouldDisableUpdateButton()).toBeTruthy();
+    });
+
+    fit('shouldDisableClearButton(): not text input, default dataset selected, no graph plotted', () => {
+        component.textInput.textInput = "";
+        component.textInputUpdate.textInput = "";
+        component.graphIsEmpty = true;
+        component.dataset = component.defaultDataset;
+        fixture.detectChanges();
+        expect(component.shouldDisableClearButton()).toBeTruthy();
+    });
+
+    fit('shouldDisableClearButton(): not text input, default dataset selected, no graph plotted, but text in update field', () => {
+        component.textInput.textInput = "";
+        component.textInputUpdate.textInput = "test";
+        component.graphIsEmpty = true;
+        component.dataset = component.defaultDataset;
+        fixture.detectChanges();
+        expect(component.shouldDisableClearButton()).toBeFalsy();
+    });
+
+    fit('shouldDisableClearButton(): not text input, default dataset selected, no graph plotted, but text in plot field', () => {
+        component.textInput.textInput = "test";
+        component.textInputUpdate.textInput = "";
+        component.graphIsEmpty = true;
+        component.dataset = component.defaultDataset;
+        fixture.detectChanges();
+        expect(component.shouldDisableClearButton()).toBeFalsy();
+    });
+
+    fit('shouldDisableClearButton(): not text input, default dataset selected, but graph plotted', () => {
+        component.textInput.textInput = "";
+        component.textInputUpdate.textInput = "";
+        component.graphIsEmpty = false;
+        component.dataset = component.defaultDataset;
+        fixture.detectChanges();
+        expect(component.shouldDisableClearButton()).toBeFalsy();
+    });
+
+    fit('shouldDisableClearButton(): not text input, no graph plotted, but not default dataset selected', () => {
+        component.textInput.textInput = "";
+        component.textInputUpdate.textInput = "";
+        component.graphIsEmpty = true;
+        component.dataset = new Dataset(-5, "test");
+        fixture.detectChanges();
+        expect(component.shouldDisableClearButton()).toBeFalsy();
+    });
+
+    fit('shouldDisableClearButton(): mixture of all', () => {
+        component.textInput.textInput = "test";
+        component.textInputUpdate.textInput = "test";
+        component.graphIsEmpty = false;
+        component.dataset = new Dataset(-5, "test");
+        fixture.detectChanges();
+        expect(component.shouldDisableClearButton()).toBeFalsy();
+
+        component.textInput.textInput = "test";
+        component.textInputUpdate.textInput = "test";
+        component.graphIsEmpty = true;
+        component.dataset = component.defaultDataset;
+        fixture.detectChanges();
+        expect(component.shouldDisableClearButton()).toBeFalsy();
+    });
+
+    fit('disableUpdateTextField()', () => {
+        component.graphIsEmpty = true;
+        fixture.detectChanges();
+        expect(component.disableUpdateTextField()).toBeTruthy();
+
+        component.graphIsEmpty = false;
+        fixture.detectChanges();
+        expect(component.disableUpdateTextField()).toBeFalsy();
+    });
+
+    fit('textIsEmpty()', () => {
+        let textInput = new TextInputComponent();
+        textInput.textInput = "";
+        fixture.detectChanges();
+        expect(component.textIsEmpty(textInput)).toBeTruthy();
+
+        textInput.textInput = "test";
+        fixture.detectChanges();
+        expect(component.textIsEmpty(textInput)).toBeFalsy();
+    });
+
+    fit('clearAll()', () => {
+        component.graphIsEmpty = false;
+        component.textInput.textInput = "test";
+        component.textInputUpdate.textInput = "test";
+        component.dataset = new Dataset(-5, "test");
+        component.columns = new Columns(["test", "test"]);
+
+        component.clearAll();
+        fixture.detectChanges();
+        expect(component.graphIsEmpty).toBeTruthy();
+        expect(component.textInput.textInput).toEqual("");
+        expect(component.textInputUpdate.textInput).toEqual("");
+        expect(component.dataset).toEqual(component.defaultDataset);
+        expect(component.columns).toEqual(null);
+    });
+
+    fit('clearAll(): test on already empty values', () => {
+        component.graphIsEmpty = true;
+        component.textInput.textInput = "";
+        component.textInputUpdate.textInput = "";
+        component.dataset = component.defaultDataset;
+        component.columns = null;
+
+        component.clearAll();
+        fixture.detectChanges();
+        expect(component.graphIsEmpty).toBeTruthy();
+        expect(component.textInput.textInput).toEqual("");
+        expect(component.textInputUpdate.textInput).toEqual("");
+        expect(component.dataset).toEqual(component.defaultDataset);
+        expect(component.columns).toEqual(null);
+    });
+
+
 });
