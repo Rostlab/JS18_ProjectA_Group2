5b35f434810d108158e68f801483bd6d6cd44460
csehlke <csehlke@users.noreply.github.com>
2018-04-10 23:51:03 +0200

    Update file-upload.component.ts

--- UI/src/app/file-upload/file-upload.component.ts
+++ UI/src/app/file-upload/file-upload.component.ts
@@ -58,7 +58,7 @@ export class FileUploadComponent implements OnInit, OnDestroy {
             this.dataList.push(new Dataset(id, fileName));
             alert(response.fileName + " has been uploaded to database!");
         }else{
-            alert("Upload Failed! Please reset the database! See the Readme file.");
+            alert("Upload Failed! Please upload a file with a different name or reset the database! See the Readme file.");
         }
     }
 
