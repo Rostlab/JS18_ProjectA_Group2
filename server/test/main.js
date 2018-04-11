a8c2edab287d67a4da9dd7ffb1f828fbbbcd2ad3
JyotirmayS <j.senapati@tum.de>
2018-04-11 22:16:14 +0200

    Update main.js

--- server/test/main.js
+++ server/test/main.js
@@ -21,7 +21,7 @@ describe('## Testing NodeIndexRoute', function() {
   });
 });
 
-describe('# Test get database tables functionality', function() {
+/*describe('# Test get database tables functionality', function() {
   it('## Testing getTables', (done) => {
     //Send get request.
     //Expect status 200 OK and result should contain tables array.
@@ -59,9 +59,9 @@ describe('## Test file upload functionality', function(){
     });
 
     //Remove table if it exists in db
-    /*db.deleteTable(tableName).then(() => {
+    db.deleteTable(tableName).then(() => {
       console.log("Table removed from database"); 
-    });  */  
+    });  
   });
 
   //Upload file. Status must be 200 OK and result should contain table name.
@@ -134,14 +134,14 @@ describe('## Test file upload functionality', function(){
         console.log('file deleted successfully');
       });
 
-    });
+    });*/
 
     //Remove table if it exists in db
     /*db.deleteTable(tableName).then( () => {
       console.log("Table removed from database"); 
-    });*/     
+    });    
   });
-});
+});*/ 
 
 // More information: https://mochajs.org/
 
